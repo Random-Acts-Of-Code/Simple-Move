1d141c9bfbb368c494b8e97ce8ec009eab190a42
Collin Makohon <cmakohon@me.com>
2016-10-29 19:35:01 -0400

    added for each to server

--- index.js
+++ index.js
@@ -12,28 +12,46 @@ var studio_converter = new Converter({});
 
 var response = {};
 
-one_converter.fromFile("csv/one.csv",function(err,result){
-	response.one = result;
+one_converter.fromFile("simple-move-data/out/one.csv",function(err,result){
+    response.one = {};
+    result.forEach(function (obj) {
+       response.one[obj.ZIP_CODE] = obj.RENT_INDEX;
+    });
 });
 
-two_converter.fromFile("csv/two.csv",function(err,result){
-	response.two = result;
+two_converter.fromFile("simple-move-data/out/two.csv",function(err,result){
+    response.two = {};
+    result.forEach(function (obj) {
+        response.two[obj.ZIP_CODE] = obj.RENT_INDEX;
+    });
 });
 
-three_converter.fromFile("csv/three.csv",function(err,result){
-	response.three = result;
+three_converter.fromFile("simple-move-data/out/three.csv",function(err,result){
+    response.three = {};
+    result.forEach(function (obj) {
+        response.three[obj.ZIP_CODE] = obj.RENT_INDEX;
+    });
 });
 
-four_converter.fromFile("csv/four.csv",function(err,result){
-	response.four = result;
+four_converter.fromFile("simple-move-data/out/four.csv",function(err,result){
+    response.four = {};
+    result.forEach(function (obj) {
+        response.four[obj.ZIP_CODE] = obj.RENT_INDEX;
+    });
 });
 
-five_converter.fromFile("csv/five.csv",function(err,result){
-	response.five = result;
+five_converter.fromFile("simple-move-data/out/five.csv",function(err,result){
+    response.five = {};
+    result.forEach(function (obj) {
+        response.five[obj.ZIP_CODE] = obj.RENT_INDEX;
+    });
 });
 
-studio_converter.fromFile("csv/studio.csv",function(err,result){
-	response.studio = result;
+studio_converter.fromFile("simple-move-data/out/studio.csv",function(err,result){
+    response.studio = {};
+    result.forEach(function (obj) {
+        response.studio[obj.ZIP_CODE] = obj.RENT_INDEX;
+    });
 });
 
 app.get('/csv', function (req, res) {
