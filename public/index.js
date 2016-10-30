0691b6e127602bc7469a40b0c44cb4df1d73b981
nepeckman <elijahpeckman@gmail.com>
2016-10-29 20:06:10 -0400

    Working alternation between fields

--- public/index.js
+++ public/index.js
@@ -11,7 +11,7 @@ require(["dojo/_base/array",
 	var map = new Map("map", {
 		basemap: "topo",
 		center: [-73.950, 40.702],
-		zoom: 10
+		zoom: 11
 	});
     
     var zips = new FeatureLayer("https://services1.arcgis.com/1L9nSO7QmA3AYgYY/arcgis/rest/services/zip_codes/FeatureServer/0",
@@ -19,20 +19,67 @@ require(["dojo/_base/array",
     
     map.addLayer(zips);
     
-    function drawFeatureLayer(data)
+    var prices = EsriRequest({
+        url: "http://localhost:3000/csv",
+        handleAs: "json"
+    });
+    prices.then(loadData);
+    
+    function loadData(data)
     {
+        window.data = data;
+        drawFeatureLayer("one");
+    }
+    
+    function drawFeatureLayer(field)
+    {
+        data = window.data[field];
+        var min = minValue(data, "RENT_INDEX");
+        var max = maxValue(data, "RENT_INDEX");
+        var breaks = calcBreaks(min, max, 4);
         var outline = new SLS("solid", new Color("#444"), 1);
         var br = new ClassBreaksRenderer(null, function(g){
-            return (g.attributes.GEOid2 == 10307) ? 20 : 5;
+            return data[g.attributes.GEOid2];
         });
-        br.addBreak(0, 6, new SFS("solid", outline, new Color([255, 255, 178, 0.75])));
-        br.addBreak(6, 1000000, new SFS("solid", outline, new Color([254, 204, 92, 0.75])));
+        br.addBreak(breaks[0], breaks[1], new SFS("solid", outline, new Color("#d8e8f3")));
+        br.addBreak(breaks[1], breaks[2], new SFS("solid", outline, new Color("#9fc5e0")));
+        br.addBreak(breaks[2], breaks[3], new SFS("solid", outline, new Color("#5296c7")));
+        br.addBreak(breaks[3], max, new SFS("solid", outline, new Color("#2c6187")));
+        
         zips.setRenderer(br);
         zips.redraw();
     }
-    var prices = EsriRequest({
-        url: "http://localhost:3000/test/data.json",
-        handleAs: "json"
-    });
-    prices.then(drawFeatureLayer);
+    
+    function minValue(obj)
+    {
+        var min = Infinity;
+        for(var key in obj){
+            if(obj[key] < min){
+                min = obj[key];
+            }
+        }
+        return min;
+    }
+    
+    function maxValue(obj)
+    {
+        var max = -Infinity;
+        for(var key in obj){
+            if(obj[key] > max){
+                max = obj[key];
+            }
+        }
+        return max;
+    }
+    
+    function calcBreaks(min, max, n)
+    {
+        var range = (max - min) / n;
+        var breakValues = [];
+        for (var i = 0; i < n; i++)
+            {
+                breakValues[i] = min + (range * i);
+            }
+        return breakValues;
+    }
 });
